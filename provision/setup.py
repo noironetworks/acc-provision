d8e285b270101bf9442bc887f5b62446bf89e62a
Gaurav Dalvi <Gaurav.Dalvi@users.noreply.github.com>
2019-04-08 17:28:41 -0700

    Updating versions for ACI 4.1.1.2 release

--- provision/setup.py
+++ provision/setup.py
@@ -6,7 +6,7 @@ from gitversion.gitversion import get_git_version
 
 setup(
     name='acc_provision',
-    version='4.1.1.1',
+    version='4.1.1.2',
     description='Tool to provision ACI for ACI Containers Controller  Build info: ' + get_git_version(),
     author="Cisco Systems, Inc.",
     author_email="apicapi@noironetworks.com",
