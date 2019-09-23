3936e6a3a4f9ae3e1af16f1013b4fadbbad696f9
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2019-09-22 22:09:55 -0700

    Adding kubeconfig option for acc-retrieve-cert
    Also, fixing incorrect config name in a comment, and,
    adding additional info message in acc-provision output
    for clarification (regarding deleting stale objects).

--- provision/acc_provision/acc_retrieve_cert.py
+++ provision/acc_provision/acc_retrieve_cert.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 
+from __future__ import print_function, unicode_literals
+
+import argparse
 import base64
 import json
 import subprocess
+import sys
+
+
+KUBECONFIG = None
+
+
+def info(msg):
+    print("INFO: " + msg, file=sys.stderr)
+
+
+def err(msg):
+    print("ERR:  " + msg, file=sys.stderr)
 
 
 def kubectl(kind, name, namespace=None):
@@ -11,6 +26,8 @@ def kubectl(kind, name, namespace=None):
     cmd.extend([kind, name])
     if namespace:
         cmd.extend(['-n', namespace])
+    if KUBECONFIG:
+        cmd.extend(['--kubeconfig', KUBECONFIG])
     retstr = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     if retstr:
         ret = json.loads(retstr).get('data')
@@ -41,11 +58,49 @@ def retrieve_certs(sysid, name, namespace=None):
     for k, v in zip(['key', 'crt'], [key, crt]):
         if v:
             fname = 'user-%s.%s' % (sysid, k)
-            with open(fname, "w") as fd:
-                fd.write(v)
+            try:
+                with open(fname, "w") as fd:
+                    fd.write(v)
+            except Exception:
+                err("Could not write: " + fname)
+                return
+            info("Retrieved: " + fname)
 
 
-def main():
+class CustomFormatter(argparse.HelpFormatter):
+    def _format_action_invocation(self, action):
+        ret = super(CustomFormatter, self)._format_action_invocation(action)
+        ret = ret.replace(' ,', ',')
+        ret = ret.replace(' file,', ',')
+        return ret
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Retrieve certificate and key added by acc-provision',
+        formatter_class=CustomFormatter,
+    )
+    parser.add_argument(
+        '-k', '--kubeconfig', default=None, metavar='file',
+        help='Path to the kubeconfig file if different from default')
+
+    return parser.parse_args()
+
+
+def main(args=None):
+    args = parse_args()
+
+    # This is ugly and temporary
+    global KUBECONFIG
+    if args.kubeconfig:
+        KUBECONFIG = args.kubeconfig
+        try:
+            open(KUBECONFIG, "r").read()
+        except Exception as e:
+            err(repr(e))
+            return
+        info("Using kubeconfig: " + KUBECONFIG)
+
     namespace_os = 'aci-containers-system'
     namespace_kube = 'kube-system'
     config_name = 'aci-containers-config'
@@ -61,8 +116,8 @@ def main():
             if sysid:
                 retrieve_certs(sysid, secret_name, namespace_kube)
         except Exception as e:
-            print(e)
-            print("Couldn't find the required secret files in either aci-containers-system or kube-system.")
+            err(repr(e))
+            err("Couldn't find the required secret files in either aci-containers-system or kube-system.")
 
 
 if __name__ == '__main__':
