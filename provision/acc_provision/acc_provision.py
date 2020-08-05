a6b56d2265c9f78fddf1f7d4b184fbbb001b6be8
Apoorva <apomitta@apoorva-vm1.local.lan>
2020-08-04 23:21:52 -0700

    Fix UT for esx flavor
    Revisit changes

--- provision/acc_provision/acc_provision.py
+++ provision/acc_provision/acc_provision.py
@@ -1644,7 +1644,7 @@ def provision(args, apic_file, no_random):
     gen = flavor_opts.get("template_generator", generate_kube_yaml)
     gen(config, output_file, output_tar, operator_cr_output_file)
 
-    if (config['flavor'] == "openshift-4.4-esx"):
+    if (config['flavor'] == "openshift-4.4-esx" and prov_apic is not None):
         apic = get_apic(config)
         nested_vswitch_vlanpool = apic.get_vmmdom_vlanpool_tDn(config['aci_config']['vmm_domain']['nested_inside']['name'])
         config['aci_config']['vmm_domain']['nested_inside']['vlan_pool'] = nested_vswitch_vlanpool
