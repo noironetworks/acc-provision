a68bb8b9e001bdbacff726048442940b4366a754
ABHISHEK SHARMA <abhisheksharmay2k@gmail.com>
2021-06-29 23:57:26 +0530

    Adding openshift-4.7-openstack flavor for OCP-4.7 in acc-provision

--- provision/acc_provision/test_main.py
+++ provision/acc_provision/test_main.py
@@ -291,6 +291,16 @@ def test_flavor_openshift_45_openstack():
         overrides={"flavor": "openshift-4.5-openstack"}
     )
 
+@in_testdir
+def test_flavor_openshift_47_openstack():
+    run_provision(
+        "flavor_openshift_47_openstack.inp.yaml",
+        "flavor_openshift_47_openstack.kube.yaml",
+        "flavor_openshift_47_openstack_tar",
+        None,
+        "flavor_openshift_47_openstack.apic.txt",
+        overrides={"flavor": "openshift-4.7-openstack"}
+    )
 
 @in_testdir
 def test_flavor_openshift_46_openstack():
