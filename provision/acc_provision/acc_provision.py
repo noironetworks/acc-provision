dd347f3e3cab3ffb9d10375263694c2d1be9e043
PariyaAshok <ashok.pariya@oneconvergence.com>
2022-06-01 11:06:31 +0530

    backport ocp flavors and other commits.

--- provision/acc_provision/acc_provision.py
+++ provision/acc_provision/acc_provision.py
@@ -1423,6 +1423,16 @@ def check_overlapping_subnets(config):
     return True
 
 
+def check_image_pull_secret(config):
+    # Check if the image_pull_secret is valid
+    image_pull_secret = config["registry"]["image_pull_secret"]
+    # This is the regex used by kubectl to validate objects names
+    pattern = "[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*"
+    if not re.fullmatch(pattern, str(image_pull_secret)):
+        return False
+    return True
+
+
 def provision(args, apic_file, no_random):
     config_file = args.config
     output_file = args.output
@@ -1565,6 +1575,12 @@ def provision(args, apic_file, no_random):
     if not check_overlapping_subnets(config):
         err("overlapping subnets found in configuration input file")
         return False
+
+    # Verify that image_pull_secret is a valid K8s secret name and not a YAML string
+    if "registry" in config.keys() and "image_pull_secret" in config["registry"]:
+        if not check_image_pull_secret(config):
+            err("Invalid image_pull_secret value, it must be a valid DNS subdomain name.")
+            return False
 
     # Adjust config based on convention/apic data
     adj_config = config_adjust(args, config, prov_apic, no_random)
