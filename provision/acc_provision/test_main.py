9a913ac72e491283ad8e65e26f03c21b7575b53d
Alok <ialokverma89@gmail.com>
2022-07-14 07:15:21 -0400

    add flavor k8s_1.24

--- provision/acc_provision/test_main.py
+++ provision/acc_provision/test_main.py
@@ -979,7 +979,7 @@ def get_args(**overrides):
         "timeout": None,
         "debug": True,
         "list_flavors": False,
-        "flavor": "kubernetes-1.23",
+        "flavor": "kubernetes-1.24",
         "version_token": "dummy",
         "release": False,
         "test_data_out": None,
