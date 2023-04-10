2ad1169df5f333df010de48046c5c19ea4fa7e4e
snaiksat <sumitnaiksatam@gmail.com>
2023-04-10 00:41:41 -0400

    Bump release to 5.2.3.7

--- provision/setup.py
+++ provision/setup.py
@@ -23,7 +23,7 @@ else:
 
 setup(
     name='acc_provision',
-    version='5.2.3.6',
+    version='5.2.3.7',
     description='Tool to provision ACI for ACI Containers Controller  Build info: ' + get_git_version(),
     author="Cisco Systems, Inc.",
     author_email="apicapi@noironetworks.com",
