64216f35a8c9b71df514d8201a5a08243f4faadb
siva-muni <siva.muni@oneconvergence.com>
2023-05-18 06:49:29 -0400

    Added RKE-1.3.21 flavor

--- provision/acc_provision/test_main.py
+++ provision/acc_provision/test_main.py
@@ -1022,6 +1022,18 @@ def test_flavor_RKE_1_4_6_base():
 
 
 @in_testdir
+def test_flavor_RKE_1_3_21_base():
+    run_provision(
+        "flavor_RKE_1_3_21.inp.yaml",
+        "flavor_RKE_1_3_21.rke.yaml",
+        None,
+        None,
+        "flavor_RKE_1_3_21.apic.txt",
+        overrides={"flavor": "RKE-1.3.21"}
+    )
+
+
+@in_testdir
 def test_sample():
     with tempfile.NamedTemporaryFile("wb") as tmpout:
         sys.stdout = tmpout
