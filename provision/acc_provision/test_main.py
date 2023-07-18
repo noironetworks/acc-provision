46fa3206bf23a7d3c6b8013dc6d06fa3c4b3b0b3
akhila <akhila.mohanan@oneconvergence.com>
2023-07-18 02:43:26 -0400

    Added support for flavor k8s 1.26
    (cherry picked from commit f769c24ef6c3a17e071799962873bb619e5853f7)

--- provision/acc_provision/test_main.py
+++ provision/acc_provision/test_main.py
@@ -1567,7 +1567,7 @@ def get_args(**overrides):
         "timeout": None,
         "debug": True,
         "list_flavors": False,
-        "flavor": "kubernetes-1.25",
+        "flavor": "kubernetes-1.26",
         "version_token": "dummy",
         "release": False,
         "test_data_out": None,
