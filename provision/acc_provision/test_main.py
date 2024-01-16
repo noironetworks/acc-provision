362598414262af123588c0ec97ce2eb256394486
PariyaAshok <ashok.pariya@oneconvergence.com>
2024-01-16 14:49:29 +0530

    add ocp 4.14 flavors for esx and bare metal.

--- provision/acc_provision/test_main.py
+++ provision/acc_provision/test_main.py
@@ -742,6 +742,18 @@ def test_flavor_openshift_413_esx():
 
 
 @in_testdir
+def test_flv_openshift_414_esx():
+    run_provision(
+        "flavor_openshift_414_esx.inp.yaml",
+        "flavor_openshift_414_esx.kube.yaml",
+        "flavor_openshift_414_esx_tar",
+        None,
+        "flavor_openshift_414_esx.apic.txt",
+        overrides={"flavor": "openshift-4.14-esx"}
+    )
+
+
+@in_testdir
 def test_flavor_openshift_414_agent_based_baremetal():
     run_provision(
         "flavor_openshift_414_agent_based_baremetal.inp.yaml",
@@ -750,6 +762,18 @@ def test_flavor_openshift_414_agent_based_baremetal():
         None,
         "flavor_openshift_414_agent_based_baremetal.apic.txt",
         overrides={"flavor": "openshift-4.14-agent-based-baremetal"}
+    )
+
+
+@in_testdir
+def test_flv_openshift_414_baremetal():
+    run_provision(
+        "flavor_openshift_414_baremetal.inp.yaml",
+        "flavor_openshift_414_baremetal.kube.yaml",
+        "flavor_openshift_414_baremetal_tar",
+        None,
+        "flavor_openshift_414_baremetal.apic.txt",
+        overrides={"flavor": "openshift-4.14-baremetal"}
     )
 
 
