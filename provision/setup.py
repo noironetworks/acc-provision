a02915c77861973bd542c96442f55a1953b69cfb
Abhishek Sharma <abhisheksharmay2k@gmail.com>
2024-02-08 19:09:25 -0800

    Bump release on lmr-6.0.3 branch to 6.0.3.4 Stable branch lmr-6.0.3 was created for 6.0.3.x releases

--- provision/setup.py
+++ provision/setup.py
@@ -23,7 +23,7 @@ else:
 
 setup(
     name='acc_provision',
-    version='6.0.3.3',
+    version='6.0.3.4',
     description='Tool to provision ACI for ACI Containers Controller  Build info: ' + get_git_version(),
     author="Cisco Systems, Inc.",
     author_email="apicapi@noironetworks.com",
